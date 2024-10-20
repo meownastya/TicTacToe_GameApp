de570dd66ca682367213ae9ef27849c6687f4521
Настя <teres.anasteisha@gmail.com>
2024-10-20 18:34:36 +0300

    я создала массив ячеек и интерфейс кнопок

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkRestart = new System.Windows.Forms.Button();
+            this.checkRules = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(868, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // checkRestart
+            // 
+            this.checkRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkRestart.Location = new System.Drawing.Point(884, 711);
+            this.checkRestart.Name = "checkRestart";
+            this.checkRestart.Size = new System.Drawing.Size(184, 51);
+            this.checkRestart.TabIndex = 1;
+            this.checkRestart.Text = "Новая игра";
+            this.checkRestart.UseVisualStyleBackColor = true;
+            // 
+            // checkRules
+            // 
+            this.checkRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkRules.Location = new System.Drawing.Point(884, 644);
+            this.checkRules.Name = "checkRules";
+            this.checkRules.Size = new System.Drawing.Size(184, 51);
+            this.checkRules.TabIndex = 2;
+            this.checkRules.Text = "Правила игры";
+            this.checkRules.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1080, 795);
+            this.Controls.Add(this.checkRules);
+            this.Controls.Add(this.checkRestart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Крестики-Нолики";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button checkRestart;
+        private System.Windows.Forms.Button checkRules;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,26 @@ namespace TicTacToe_GameApp
 {
     public partial class Form1 : Form
     {
+        private int x = 12, y = 12;
+        private Button[,] buttons = new Button[3, 3];
+        private int player;
+        bool winButton = false;
         public Form1()
         {
             InitializeComponent();
+            this.Height = 680;
+            this.Width = 850;
+            player = 1;
+            label1.Text = "Текущий ход: Игрок 1";
+            for (int i = 0; i < buttons.Length / 3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(200, 200);
+                }
+            }
+            setButtons();
         }
     }
 }
